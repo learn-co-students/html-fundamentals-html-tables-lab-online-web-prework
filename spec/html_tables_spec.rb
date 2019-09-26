cbdcad698b2ac43edab74700e55f341f899fa659
Prince Charles <princecharles35@yahoo.com>
2019-09-26 18:55:22 +0000

    Done.

--- spec/html_tables_spec.rb
+++ spec/html_tables_spec.rb
@@ -1,55 +1,22 @@
-require 'pry'
-
-RSpec.describe 'index.html' do
-  it 'contains a <table> with five rows' do
-    table = parsed_html.search('table').first
-    expect(table).to_not be_nil, "The main <table> tag is missing!"
-    expect(html_file_contents).to include('</table>'), "Don't forget to include a closing </table>"
-
-
-    children = table.children.select {|child| child.name == "tr"}
-    expect(children.length).to be == 5, "Your <table> tag needs five <tr> tags, one for each row"
-
-  end
-
-  it 'contains a <table> with the correct headers' do
-    headers = parsed_html.search('table > tr').first
-
-    expect(headers.children[0].children[0].text).to eq("City")
-    expect(headers.children[2].children[0].text).to eq("State")
-    expect(headers.children[4].children[0].text).to eq("2017 estimate")
-  end
-
-  it 'contains the correct information about New York City' do
-    new_york = parsed_html.search('table > tr')[1]
-
-    expect(new_york.children[0].children[0].text).to eq("New York")
-    expect(new_york.children[2].children[0].text).to eq("New York")
-    expect(new_york.children[4].children[0].text).to be == "8,622,698"
-  end
-
-  it 'contains the correct information about California' do
-    new_york = parsed_html.search('table > tr')[2]
-
-    expect(new_york.children[0].children[0].text).to eq("Los Angeles")
-    expect(new_york.children[2].children[0].text).to eq("California")
-    expect(new_york.children[4].children[0].text).to be == "3,999,759"
-  end
-
-  it 'contains the correct information about Chicago' do
-    new_york = parsed_html.search('table > tr')[3]
-
-    expect(new_york.children[0].children[0].text).to eq("Chicago")
-    expect(new_york.children[2].children[0].text).to eq("Illinois")
-    expect(new_york.children[4].children[0].text).to be == "2,716,450"
-  end
-
-  it 'contains the correct information about Houston' do
-    new_york = parsed_html.search('table > tr')[4]
-
-    expect(new_york.children[0].children[0].text).to eq("Houston")
-    expect(new_york.children[2].children[0].text).to eq("Texas")
-    expect(new_york.children[4].children[0].text).to be == "2,312,717"
-  end
-
-end
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Phone Number</th>
+    <th>Bringing</th>
+  </tr>
+  <tr>
+    <td>Daniel</td>
+    <td>718-555-1212</td>
+    <td>Guacamole</td>
+  </tr>
+  <tr>
+    <td>Rachel</td>
+    <td>718-555-1212</td>
+    <td>Chips</td>
+  </tr>
+  <tr>
+    <td>Terrance</td>
+    <td>914-555-1212</td>
+    <td>Salsa</td>
+  </tr>
+</table>
